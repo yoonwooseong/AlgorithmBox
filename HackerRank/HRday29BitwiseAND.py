ec7b6cecf39e2a15ad508f37687773f71336c588
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-11-08 10:59:11 +0900

    Update HRday29BitwiseAND.py

--- HackerRank/HRday29BitwiseAND.py
+++ HackerRank/HRday29BitwiseAND.py
@@ -6,7 +6,17 @@ import random
 import re
 import sys
 
+if __name__ == '__main__':
+    t = int(input())
 
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+        k = int(nk[1])
+
+        print(k-1 if ((k-1) | k) <= n else k-2)
+'''
 if __name__ == '__main__':
     t = int(input())
 
@@ -24,3 +34,12 @@ if __name__ == '__main__':
                     maxnum = bitand
 
         print(maxnum)
+'''
+'''
+# 구글링해서 k가 홀수일때와 짝수일때의 비트연산자 or(|)를 사용해서 제출하는 방법을 알아냄. 
+# 정확히 이해는 안가는 부분이라 따로 학습이 필요할듯
+if (k|(k-1)) <= n:
+    print(k-1)  
+else:
+    print(k-2)
+'''
