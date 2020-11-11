02a421357ece4fcd4f6d71727d19a571a38fe3d4
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-11-11 10:52:14 +0900

    Update 크레인인형뽑기게임.py

--- 코딩테스트 연습/크레인인형뽑기게임.py
+++ 코딩테스트 연습/크레인인형뽑기게임.py
@@ -30,10 +30,11 @@ def solution(board, moves):
                         bounds.pop()
                         bounds.pop()
                         removecount += 2
-                        while bounds[-1] == bounds[-2]:
-                            bounds.pop()
-                            bounds.pop()
-                            removecount += 2
+                        if len(bounds) > 2:
+                            while bounds[-1] == bounds[-2]:
+                                bounds.pop()
+                                bounds.pop()
+                                removecount += 2
                     break
     print(removecount)
     return removecount
@@ -41,5 +42,5 @@ def solution(board, moves):
 
 # 4 3 1 1 3 2 4
 
-solution([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [
-         4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1, 5, 3, 5, 1, 2, 1, 4]	)
+solution([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [
+         1, 1, 1, 1, 1], [1, 1, 1, 1, 1]], [1, 5, 3, 5, 1, 2, 1, 4]	)
